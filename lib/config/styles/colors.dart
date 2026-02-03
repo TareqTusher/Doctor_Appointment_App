42aeecb49f96a4d897ba1c18141f44e63b096dce
rimon-nextzen <rimon@nextzen.net>
2026-02-03 15:43:35 +0600

    first commit

---
+++ lib/config/styles/colors.dart
@@ -0,0 +1,49 @@
+ import 'package:flutter/material.dart';
+
+class AppColors{
+
+  static const Color midNightBlue = Color(0xFF1C2A3A);
+    static const Color white = Color(0xFFFFFFFF);
+    
+    static const Color grey50 = Color(0xFFF9FAFB);
+        static const Color grey100 = Color(0xFFF3F4F6);
+
+    static const Color grey200 = Color(0xFFE5E7EB);
+
+    static const Color grey300 = Color(0xFFD1D5DB);
+
+    static const Color grey400 = Color(0xFF9CA3AF);
+
+    static const Color grey500 = Color(0xFF6B7280);
+        static const Color grey600 = Color(0xFF4B5563);
+
+    static const Color grey700 = Color(0xFF374151);
+
+    static const Color grey800 = Color(0xFF374151);
+
+    static const Color grey900 = Color(0xFF111928);
+
+    static const Color deepTeal = Color(0xFF014737);
+
+    static const Color teal = Color(0xFF4D9B91);
+
+    static const Color lightTeal = Color(0xFFA4CFC3);
+
+
+
+    static const Color green = Color(0xFF93C19E);
+    static const Color paleGreen = Color(0xFFDEF7E4);
+    static const Color darkRed = Color(0xFF771D1D);
+    static const Color deepPink = Color(0xFFDC9497);
+
+    static const Color pink = Color(0xFFDEB6B5);
+    static const Color lightPink = Color(0xFFFDE8E8);
+    static const Color lightPurple = Color(0xFFACA1CD);
+    static const Color blue = Color(0xFF1C64F2);
+    static const Color paleBlue = Color(0xFF89CCDB);
+    static const Color purple = Color(0xFF352261);
+    static const Color orange = Color(0xFFDC9497);
+
+
+
+ }
